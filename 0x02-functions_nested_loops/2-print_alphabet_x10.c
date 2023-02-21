511481ab60f89f0a590b1254c41ae377db1c99f4
Chisom <metusommy@gmail.com>
2023-02-21 07:31:11 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet 10times in lowercase
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int b = 0;
+
+	while (b <= 9)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+		b++;
+	}
+}
