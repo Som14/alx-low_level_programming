c718c53920d246e37dcdc177417d094c903a54d5
Chisom <metusommy@gmail.com>
2023-03-16 10:27:17 -0700

    task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,7 +11,7 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned long nul;
+	unsigned long mul;
 	int i, j;
 
 	if (argc != 3)
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
-	nul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", nul);
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
 	return (0);
 }
